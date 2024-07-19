c66bc83979d04b040de9d80cff4ca2b163d4092b
webster-rafael <webster.rafael2014@gmail.com>
2024-07-18 23:48:07 -0400

    Last commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Services from "./components/Services";
 function App() {
   return (
     <main className="w-full relative">
-      <a className="" href="https://api.whatsapp.com/send?phone=5521978737571&text=Ol%C3%A1,%20tenho%20uma%20d%C3%BAvida!" target="_blank">
+      <a className="z-50" href="https://api.whatsapp.com/send?phone=5521978737571&text=Ol%C3%A1,%20tenho%20uma%20d%C3%BAvida!" target="_blank">
         <RiWhatsappFill className="text-green-700 fixed bottom-4 size-16 right-4 animate-bounce cursor-pointer" />
       </a>
 
